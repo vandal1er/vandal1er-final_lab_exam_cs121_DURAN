fa70eeceaaf2c16e9804158cab14243ed434688c
vandal1er <23-01394@g.batstate-u.edu.ph>
2024-05-10 14:13:51 +0800

    registration done

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ game = dice_game.DiceGame()
 manager = user_manager.UserManager()
 
 def run():
-    base.Cls()
-    base.DrawLine(26)
-    print("Welcome to Dice Roll Game!")
-    print("1. Register\n2.Login\n3.Exit")
-        
-    choice = base.GetInput(3)
-        
-    if choice == 1:
-        pass
+    while True:
+        base.Cls()
+        base.DrawLine(26)
+        print("Welcome to Dice Roll Game!")
+        print("1. Register\n2. Login\n3. Exit")
+            
+        choice = base.GetInput(3)
+            
+        if choice == 1:
+            manager.register()
     
 run()
--- utils/user_manager.py
+++ utils/user_manager.py
@@ -17,10 +17,30 @@ class UserManager:
         pass
 
     def register(self):
-        username = input("Username: ")
-        if username in self.accounts:
-            pass
+        while True:
+            Cls()
+            DrawLine(26)
+            print("Account Registration\n")
+            username = input("Username: ")
+            if username in self.accounts:
+                Pause("Username already exists.")
+                continue
+            if len(username) < 4 and len(username) > 0:
+                Pause("Username must be at least 4 characters long.")
+                continue
+            if username == "":
+                return
             
+            pw = input("Password: ")
+            if len(pw) < 8 and len(pw) > 0:
+                Pause("Password must be at least 8 characters long.")
+                continue
+            if pw == "":
+                return
+            
+            self.accounts[username] = User(username, pw)
+            Pause("Account registration successful!")
+            return
         
 
     def login():
